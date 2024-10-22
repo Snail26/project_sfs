93fa7b1f3519e92861712f60b1877bed6f11faae
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-10-22 21:30:41 +1300

    added update script

--- setup.bat
+++ setup.bat
@@ -1,3 +1,3 @@
 pip3 install cryptography
-curl -o _access.py --ssl-no-revoke 
+curl -o _access.py --ssl-no-revoke https://raw.githubusercontent.com/Snail26/project_sfs/refs/heads/main/_access.py
 del setup.bat
---
+++ update.bat
@@ -0,0 +1,1 @@
+curl -o _access.py --ssl-no-revoke https://raw.githubusercontent.com/Snail26/project_sfs/refs/heads/main/_access.py
