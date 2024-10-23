2886a9ceec363857a1eb42f40fe87903b9ac6ff6
Snail26 <141910498+Snail26@users.noreply.github.com>
2024-10-23 17:23:17 +1300

    fixed

--- _access.py
+++ _access.py
@@ -1,11 +1,9 @@
 from cryptography.fernet import Fernet
-import hashlib
 import os
 import base64
 
 option = input("0 to decrypt. 1 to encrypt: ")
-password = input("Password: ")
-upwd = password
+password = input("Password: ")[0: 32]
 pwdl = len(password)
 
 for i in range(32 - pwdl):
@@ -13,8 +11,6 @@ for i in range(32 - pwdl):
 
 password = password.encode("UTF-8")
 password = base64.b64encode(password)
-
-open("./unlocked/lyp.key").close()
 
 if option == "0":
     files = os.listdir("./locked")
@@ -29,13 +25,9 @@ if option == "0":
             with open("./unlocked/" + fileName, "wb") as writeFile:
                 writeFile.write(contentsE)
                 writeFile.close()
-            with open("./unlocked/lyp.key") as writeFile:
-                writeFile.write(upwd)
-                writeFile.close()
                 
 if option == "1":
     files = os.listdir("./unlocked")
-    os.remove("./unlocked/ltp.key")
     for fileName in files:
         with open("./unlocked/" + fileName, "rb") as file:
             contents = file.read()
